851cfb90889de14702fce2db5a6f5577024fa2da
yongcogiulio125@gmail.com <yongcogiulio125@gmail.com>
2023-11-12 19:57:40 +0800

    changed the c template

--- C00_misc.h
+++ C00_misc.h
@@ -5,14 +5,7 @@
 // =======================================================================
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-
-// =======================================================================
-// STRUCTS 1
-// =======================================================================
-
-typedef enum boolean{FALSE, TRUE}BOOLEAN;
 
 // =======================================================================
 // MISC FUNCTIONS 1
@@ -37,7 +30,7 @@ void separator(char* message){
 void error_in(char* function_name){
 	int count = 53; 				//additional 3 for the (\n)s and the \0
 	char new_string[count];
-	char template[] = "Error in function: ";
+	char error_teplate[] = "Error in function: ";
 
 	memset(new_string, '-', count);
 
@@ -45,14 +38,11 @@ void error_in(char* function_name){
 	new_string[count-2] = '\n';
 	new_string[count-1] = '\0';
 
-	memcpy(new_string + 1, template, strlen(template));
-	memcpy(new_string + 1 + strlen(template), function_name, strlen(function_name));
+	memcpy(new_string + 1, error_teplate, strlen(error_teplate));
+	memcpy(new_string + 1 + strlen(error_teplate), function_name, strlen(function_name));
 
-	new_string[strlen(template) + strlen(function_name) + 1] = ' ';
+	new_string[strlen(error_teplate) + strlen(function_name) + 1] = ' ';
 
 	printf("%s", new_string);
 }
 
-// =======================================================================
-// CORE FUNCTIONS 1
-// =======================================================================
